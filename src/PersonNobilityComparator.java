500f989e84f657746f7d99777c9bb29764e2fc62
Maksim Kalashnikov <maksim-kalashnikov-2002@mail.ru>
2023-03-16 18:56:31 +0300

    fix

--- src/PersonNobilityComparator.java
+++ src/PersonNobilityComparator.java
@@ -1,7 +1,7 @@
 import java.util.Comparator;
 
 public class PersonNobilityComparator implements Comparator<Person> {
-    private int maxSurname = Integer.MAX_VALUE;
+    private final int maxSurname;
 
     public PersonNobilityComparator(int maxSurname) {
         this.maxSurname = maxSurname;
@@ -9,7 +9,7 @@ public class PersonNobilityComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        if ((Integer.compare(o1.getSurname().split(" ").length, o2.getSurname().split(" ").length) == 0) || (o1.getSurname().split(" ").length > maxSurname) || (o2.getSurname().split(" ").length > maxSurname)) {
+        if ((o1.getSurname().split(" ").length == o2.getSurname().split(" ").length) || (o1.getSurname().split(" ").length > maxSurname) || (o2.getSurname().split(" ").length > maxSurname)) {
             return Integer.compare(o1.getAge(), o2.getAge());
         } else {
             return Integer.compare(o1.getSurname().split(" ").length, o2.getSurname().split(" ").length);
